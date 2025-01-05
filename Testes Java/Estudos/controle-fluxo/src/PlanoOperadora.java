acfb86cdf7e6f41f13b7ca79b9fcc9a8bab15db9
ferncristin2 <106873254+ferncristin2@users.noreply.github.com>
2025-01-05 15:09:23 -0300

    Concluindo mórulo Estruturas condiçionais

--- Testes Java/Estudos/controle-fluxo/src/PlanoOperadora.java
+++ Testes Java/Estudos/controle-fluxo/src/PlanoOperadora.java
@@ -2,18 +2,30 @@ public class PlanoOperadora {
 
     public static void main(String[] args) {
         
-        String plano = "B";
+        String plano = "T";
+        
+        switch (plano) {
+            case "T":{
+                System.out.println("5Gb Youtube");
+            }
+            case "M":{
+            System.out.println("Whats e Instragram grátis");
+            }
+            case "B":{
+            System.out.println("100 minutos de ligações");
+            }    
+        }    
+    }
+}
 
+/* EXEMPLO DE COMO PODRIA SER FEITO
         if(plano == "B"){
             System.out.println("100 minutos de ligação");
         }else if(plano == "M");{
             System.out.println("100 minutos de ligação");
             System.out.println("Whats e Instagram Grátis");
-        }else if(plano == "T");{
+        }else if(plano == "M");{  ---   ESSE ÚLTIMO ELSE IF APRESENTOU ERRO
             System.out.println("100 minutos de ligação");
             System.out.println("Whats e Instagram Grátis");
             System.out.println("5Gb YouTube");
-        }
-        }
-   
-}
+        }*/
