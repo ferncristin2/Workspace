c2a2aa9fef7ea29f357e32490cee586be21131d8
ferncristin2 <106873254+ferncristin2@users.noreply.github.com>
2025-01-07 07:25:25 -0300

    Treinos iniciais lógica

---
+++ Testes Logica/Testes para Logica/src/TelaDoisPokemon.java
@@ -0,0 +1,11 @@
+public class TelaDoisPokemon {
+    public static void main(String[] args) {
+        
+        String nomePokemon = {"Pikachu"}
+        int nivelPokemon = 20
+        int pontosDeVida = 45
+        String sexoPokemon = "M"
+        boolean selecionavel = true
+
+    }
+}
