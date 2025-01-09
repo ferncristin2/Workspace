3088592b50ac1f3ed3ba67e528ed1ee8e4a1321e
ferncristin2 <106873254+ferncristin2@users.noreply.github.com>
2025-01-08 22:26:31 -0300

    Correções diversas

---
+++ Testes_Logica/Testes para Logica/src/DesafioFelipao.java
@@ -0,0 +1,40 @@
+public class DesafioFelipao {
+
+    public static void main(String[] args) {
+
+        int xpHeroi=5000;
+        String nomeHeroi = "Aruathnar";
+
+        switch (xpHeroi) { 
+        case 1000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Ferro.");
+        break; }
+        case 2000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Bronze."); 
+        break;}
+        case 5000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Prata.");
+        break;}
+        case 7000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Ouro.");
+        break;}
+        case 8000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Platina."); 
+        break;}
+        case 9000:{ 
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Ascendente."); 
+        break;}
+        case 10000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Imortal."); 
+        break;}
+        case 10000:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Imortal."); 
+        break;}
+        default:{
+        System.out.println("Nosso heroi " + nomeHeroi + " tem Experiência nível Imortal.");
+    
+    }
+    }
+    }
+    }
+    
---
+++ Testes_Logica/Testes para Logica/src/SegundaTelaPokemon.java
@@ -0,0 +1,11 @@
+public class SegundaTelaPokemon {
+    public static void main(String[] args) {
+        
+        String nomePokemon = ("Pikachu");
+        int nivelPokemon = 20;
+        int pontosDeVida = 45;
+        String sexoPokemon = ("M");
+        boolean selecionavel = true;
+
+    }
+}
